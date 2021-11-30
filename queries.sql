d3a1a9d17b62fe3162b381cc0f86f914e3ee430e
Moises <mhdezcoronado@gmail.com>
2021-11-30 17:44:29 +0100

    Write some queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,30 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT name FROM animals
+  where name like '%mon%';
+
+SELECT name FROM animals
+  WHERE date_of_birth
+  BETWEEN '2016-01-01' AND '2019-01-01';
+
+SELECT name FROM animals
+  WHERE neutered
+  AND escape_attempts < 3;
+
+SELECT name FROM animals
+  WHERE name
+  IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts
+  FROM animals
+  WHERE weight_kg > 10.5;
+
+SELECT name FROM animals
+  WHERE neutered;
+
+SELECT name FROM animals
+  WHERE name != 'Gabumon';
+
+SELECT name FROM animals
+  WHERE weight_kg
+  BETWEEN 10.4 AND 17.3;
