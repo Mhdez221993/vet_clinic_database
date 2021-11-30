4722390dbe652c6a1fff4e3944cb62b64611c7f5
Moises <mhdezcoronado@gmail.com>
2021-11-30 17:08:31 +0100

    Create animals TABLE

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
-
+--=# CREATE DATABASE vet_clinic;
+-- =# UPDATE animals SET id = 2 WHERE name = 'Gabumon';
 CREATE TABLE animals (
-    name varchar(100)
+    id                int NOT NULL,
+    name              varchar(100) NOT NULL,
+    date_of_birth     date,
+    escape_attempts   integer,
+    neutered          boolean,
+    weight_kg         decimal(5,2)
 );
+
