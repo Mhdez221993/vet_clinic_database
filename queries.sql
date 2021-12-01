7a6124661c36fc2a7b5bf1c4b8ad18e5eda5c37e
Moises <mhdezcoronado@gmail.com>
2021-12-01 08:45:32 +0100

    Remove unnessessary code

--- queries.sql
+++ queries.sql
@@ -2,17 +2,14 @@
 
 SELECT * FROM animals where name like '%mon%';
 
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND 2019;
 
-SELECT name FROM animals WHERE neutered AND escape_attempts < 3;
 SELECT name FROM animals WHERE neutered Is TRUE AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
-SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE neutered IS TRUE;
 
 SELECT * FROM animals WHERE name != 'Gabumon';
