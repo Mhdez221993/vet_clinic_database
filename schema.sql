4a7c0afd7d0a3080724ecf226052f8438a05b10f
Moises <mhdezcoronado@gmail.com>
2021-12-01 07:58:09 +0100

    Remove unnessessary code

--- schema.sql
+++ schema.sql
@@ -1,6 +1,4 @@
 /* Database schema to keep the structure of entire database. */
---=# CREATE DATABASE vet_clinic;
--- =# UPDATE animals SET id = 2 WHERE name = 'Gabumon';
 CREATE TABLE animals (
     id                INT GENERATED ALWAYS AS IDENTITY,
     name              varchar(100),
