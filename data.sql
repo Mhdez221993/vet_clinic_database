66251d76f4131f8bf35cd8cc85d46687eee2d7f9
Moises <mhdezcoronado@gmail.com>
2021-12-01 07:57:33 +0100

    Optimize data

--- data.sql
+++ data.sql
@@ -1,6 +1,34 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
-INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8);
-INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04);
-INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5, TRUE, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+  'Agumon',
+  'Feb 3, 2020',
+  0,
+  true,
+  10.23
+);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+  'Gabumon',
+  'Nov 15, 2018',
+  2,
+  true,
+  8
+);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+  'Pikachu',
+  'Jan 7, 2021',
+  1,
+  FALSE,
+  15.04
+);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+  'Devimon',
+  'May 12, 2017',
+  5,
+  true,
+  11
+);
+
