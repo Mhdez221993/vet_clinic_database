5820d5a989bea00b3bd02ba90a83cb85fde24b94
Moises <mhdezcoronado@gmail.com>
2021-12-03 12:15:15 +0100

    Create owners & species table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
 
 -- Add a column species
 ALTER TABLE animals ADD species varchar(100);
+
+-- query multiple tables
+CREATE TABLE owners (
+    id                INT GENERATED ALWAYS AS IDENTITY,
+    full_name         varchar(100),
+    age               INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id                INT GENERATED ALWAYS AS IDENTITY,
+    name         varchar(100),
+    PRIMARY KEY (id)
+);
+
+-- NOTE: The id from animals table was set from the first exercise to be AUTOINCREMENTED
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT, ADD CONSTRAINT species_id FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
+ALTER TABLE animals ADD owner_id INT, ADD CONSTRAINT owner_id FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
