5f8bfa78b0d4df9c34e53104f17f56396ee97671
Moises <mhdezcoronado@gmail.com>
2021-12-03 12:15:44 +0100

    Insert data & Update animal table

--- data.sql
+++ data.sql
@@ -80,3 +80,33 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
   true,
   3
 );
+
+-- Vet clinic database: query multiple tables
+BEGIN;
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+COMMIT;
+
+BEGIN;
+INSERT INTO species (name) VALUES
+  ('Pokemon'),
+  ('Digimon');
+COMMIT;
+
+BEGIN;
+UPDATE animals SET species_id = 1 WHERE name LIKE '%mon%';
+UPDATE animals SET species_id = 2 WHERE species_id IS NULL;
+COMMIT;
+
+BEGIN;
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
+COMMIT;
