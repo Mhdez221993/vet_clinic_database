c3a90bea188d3a50996e9fc5e6c0b58b403bd4c4
Moises <mhdezcoronado@gmail.com>
2021-12-06 14:19:40 +0100

    Remove duplicate code

--- data.sql
+++ data.sql
@@ -1,85 +1,21 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-  'Agumon',
-  'Feb 3, 2020',
-  0,
-  true,
-  10.23
-);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-  'Gabumon',
-  'Nov 15, 2018',
-  2,
-  true,
-  8
-);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-  'Pikachu',
-  'Jan 7, 2021',
-  1,
-  FALSE,
-  15.04
-);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-  'Devimon',
-  'May 12, 2017',
-  5,
-  true,
-  11
-);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+  VALUES
+  ( 'Agumon', 'Feb 3, 2020', 0, true, 10.23 ),
+  ( 'Gabumon', 'Nov 15, 2018', 2, true, 8 ),
+  ( 'Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04 ),
+  ( 'Devimon', 'May 12, 2017', 5, true, 11 );
 
 -- update animals table
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
-  'Charmander',
-  'Feb 8, 2020',
-  -11,
-  false,
-  0
-);
-
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
-  'Plantmon',
-  'Nov 15, 2022',
-  -5.7,
-  true,
-  2
-);
-
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
-  'Squirtle',
-  'Apr 2, 1993',
-  -12.13,
-  false,
-  3
-);
-
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
-  'Angemon',
-  'Jun 12, 2005',
-  -45,
-  true,
-  1
-);
-
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
-  'Boarmon',
-  'Jun 7, 2005',
-  20.4,
-  true,
-  7
-);
-
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
-  'Blossom',
-  'Oct 13, 1998',
-  17,
-  true,
-  3
-);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+  VALUES
+    ( 'Charmander', 'Feb 8, 2020', -11, false, 0 ),
+    ( 'Plantmon', 'Nov 15, 2022', -5.7, true, 2 ),
+    ( 'Squirtle', 'Apr 2, 1993', -12.13, false, 3 ),
+    ( 'Angemon', 'Jun 12, 2005', -45, true, 1 ),
+    ( 'Boarmon', 'Jun 7, 2005', 20.4, true, 7 ),
+    ( 'Blossom', 'Oct 13, 1998', 17, true, 3 );
 
 -- Vet clinic database: query multiple tables
 BEGIN;
