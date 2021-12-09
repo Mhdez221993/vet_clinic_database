6e8da60ccde481d925f3613aa51301250af6fb1c
Moises <mhdezcoronado@gmail.com>
2021-12-09 12:03:42 +0100

    Fix visits table

--- schema.sql
+++ schema.sql
@@ -73,10 +73,10 @@ CREATE TABLE specializations (
 );
 
 CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
     vet_id     INT,
     animal_id  INT,
-    visit_date  DATE,
-    PRIMARY KEY (vet_id, animal_id, visit_date),
+    date_of_visit  DATE,
     CONSTRAINT fk_vet
         FOREIGN KEY (vet_id)
         REFERENCES vets (id)
@@ -88,3 +88,14 @@ CREATE TABLE visits (
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
