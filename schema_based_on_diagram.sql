e402b5c328c1ff6bd4066617f165b64533c55059
wuyepabdul <wuyepabdul@gmail.com>
2021-12-09 19:14:41 +0100

    Add foreign key to medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,38 +1,43 @@
 CREATE TABLE patients (
-  id               INT GENERATED ALWAYS AS IDENTITY,
-  name             varchar(100),
-  date_of_birth    date,
-  PRIMARY KEY (id)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    date_of_birth date,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE medical_histores (
-  id             INT GENERATED ALWAYS AS IDENTITY,
-  admitted_at    date,
-  patient_id     int,
-  status         varchar(100),
-  PRIMARY KEY (id)
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at date,
+    patient_id int,
+    status varchar(100),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE treatments (
-  id       INT GENERATED ALWAYS AS IDENTITY,
-  type     varchar(100),
-  name     varchar(100),
-  PRIMARY KEY (id)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type varchar(100),
+    name varchar(100),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE invoice_items (
-  id             INT GENERATED ALWAYS AS IDENTITY,
-  unit_price     decimal(10, 2),
-  quantity       int,
-  total_price    decimal,
-  PRIMARY KEY (id)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price decimal(10, 2),
+    quantity int,
+    total_price decimal,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE invoice (
-  id                     INT GENERATED ALWAYS AS IDENTITY,
-  total_amount           decimal(10, 2),
-  generated_at           int,
-  payed_at               timestamp,
-  medical_history_id     int,
-  PRIMARY KEY (id)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount decimal(10, 2),
+    generated_at int,
+    payed_at timestamp,
+    medical_history_id int,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE
+    medical_histories
+ADD
+    FOREIGN KEY patient_id REFERENCES patients(id);
