2b613d655467c61bd79293e41d195e017511243e
Moises <mhdezcoronado@gmail.com>
2021-12-13 12:39:13 +0100

    Add screenshopt before optimization

--- data.sql
+++ data.sql
@@ -260,7 +260,3 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id F
 
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
-
--- Check by running
-explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
--- If you get Execution time: X ms and X >= 1000: that should be enough
