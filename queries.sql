d5d647ec232e3ecd3ed5acbe44064637e5b8cbbe
Moises <mhdezcoronado@gmail.com>
2021-12-15 09:25:10 +0100

    Remove whitespaces

--- queries.sql
+++ queries.sql
@@ -313,6 +313,7 @@ SELECT species.name, COUNT(visits.animal_id)
     DESC LIMIT 1;
 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
